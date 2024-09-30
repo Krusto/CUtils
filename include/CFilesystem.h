9487934db4f8c16c8871f8efe9a3923f9d9a34b9
Krusto Stoyanov <k.stoianov2@gmail.com>
2024-09-30 18:20:06 +0300

    fixed buffer copy

--- include/CFilesystem.h
+++ include/CFilesystem.h
@@ -224,7 +224,7 @@ FileOpResultT file_read_utf8(const int8_t* filename, size_t* filesize, int8_t** 
         else
         {
             // copy the file into the buffer.
-            fread(buffer, 1, *filesize, fileIn);
+            fread(*buffer, 1, *filesize, fileIn);
         }
         fclose(fileIn);
     }
