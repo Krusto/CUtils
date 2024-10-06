9e27a01e4181a7dd848b2f8fd0b09f97ea7cd4a7
Krusto Stoyanov <k.stoianov2@gmail.com>
2024-10-06 18:20:50 +0300

    fixed unicode logging

--- include/CLog.h
+++ include/CLog.h
@@ -41,6 +41,7 @@ Includes
 ***********************************************************************************************************************/
 #ifndef NO_STD_LOG
 #include <stdio.h>
+#include <wchar.h>
 #endif
 /***********************************************************************************************************************
 Macro Definitions
@@ -70,12 +71,12 @@ Macro Definitions
 
     #define WLOG( ... ) wprintf( __VA_ARGS__ )
     #define WLOG_ERROR( ... )                                                                                               \
-        WLOG( "Error: " );                                                                                                  \
+        WLOG( L"Error: " );                                                                                                  \
         WLOG( __VA_ARGS__ )
     
     #ifdef CUTILS_VERBOSE
         #define WLOG_INFO( ... )                                                                                                \
-            WLOG( "Info: " );                                                                                                   \
+            WLOG( L"Info: " );                                                                                                   \
             WLOG( __VA_ARGS__ )
     #else   
         #define WLOG_INFO( ... )
@@ -84,7 +85,7 @@ Macro Definitions
         #define WLOG_DEBUG(...)
     #else
         #define WLOG_DEBUG(...)\
-            WLOG("Debug: ");\
+            WLOG(L"Debug: ");\
             WLOG(__VA_ARGS__)
     #endif
 
